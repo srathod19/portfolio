e77428ba3bbca0aa67f0b1713c1b0da0cd4f48a2
Sachin R <sachinrathod@hyperlinkinfosystem.net.in>
2025-10-29 15:38:56 +0530

    [MOD] minor changes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,9 @@
-export default function Footer(){
-return (
-<footer className="bg-white border-t">
-<div className="container mx-auto px-6 py-6 text-center text-sm text-gray-600">
-© {new Date().getFullYear()} Sachin — Built with Next.js
-</div>
-</footer>
-)
+export default function Footer() {
+  return (
+    <footer className="bg-white border-t">
+      <div className="container mx-auto px-6 py-6 text-center text-sm text-gray-600">
+        © {new Date().getFullYear()} Sachin — Built with Next.jslalaal
+      </div>
+    </footer>
+  );
 }
