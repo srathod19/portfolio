b6a39ee1da41c9250cfeff92e7fddd4619f0833d
Sachin R <sachinrathod@hyperlinkinfosystem.net.in>
2025-10-29 15:41:59 +0530

    [MOD] rollback

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="bg-white border-t">
       <div className="container mx-auto px-6 py-6 text-center text-sm text-gray-600">
-        © {new Date().getFullYear()} Sachin — Built with Next.jslalaal
+        © {new Date().getFullYear()} Sachin — Built with Next.js
       </div>
     </footer>
   );
